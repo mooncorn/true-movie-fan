9a9497b85c18712a4ad11f18a0ec02c93ece4264
Manlika Throngsiriwat <118219370+PuipuyManlika@users.noreply.github.com>
2022-12-07 19:04:13 -0500

    Connect UserAuth on Firebase

--- app/src/main/java/com/example/truemoviefan/LoginActivity.java
+++ app/src/main/java/com/example/truemoviefan/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.truemoviefan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initialize();
+    }
+
+    private void initialize() {
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(view -> {
+            goToRegisterPage();
+        });
+    }
+
+    private void goToRegisterPage() {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/truemoviefan/RegisterActivity.java
+++ app/src/main/java/com/example/truemoviefan/RegisterActivity.java
@@ -1,14 +1,124 @@
 package com.example.truemoviefan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUserNameSecondAct, edEmailSecondAct, edPasswordSecondAct, edConfirmPasswordSecondAct;
+    Button btnSignUp, btnGobackToLogin;
+    FirebaseAuth fAuth;
+
+    String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initialize();
     }
+
+    private void initialize() {
+        edUserNameSecondAct = findViewById(R.id.edUserNameSecondAct);
+        edEmailSecondAct = findViewById(R.id.edEmailSecondAct);
+        edPasswordSecondAct = findViewById(R.id.edPasswordSecondAct);
+        edConfirmPasswordSecondAct = findViewById(R.id.edConfirmPasswordSecondAct);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnGobackToLogin = findViewById(R.id.btnGobackToLogin);
+
+        //Firebase
+        fAuth = FirebaseAuth.getInstance();
+
+        btnSignUp.setOnClickListener(view -> {
+            registerNewUser();
+        });
+
+        btnGobackToLogin.setOnClickListener(view -> {
+            gotoLoginPage();
+        });
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+    }
+
+    private void gotoLoginPage() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    private void registerNewUser() {
+        String name = edUserNameSecondAct.getText().toString().toString();
+        String email = edEmailSecondAct.getText().toString().trim();
+        String passw = edPasswordSecondAct.getText().toString().trim();
+        String confirmPassw = edConfirmPasswordSecondAct.getText().toString().trim();
+
+        if (TextUtils.isEmpty(name)){
+            edUserNameSecondAct.setError("Username is required!");
+            return;
+        }
+
+        if (TextUtils.isEmpty(email)){
+            edEmailSecondAct.setError("Email is required!");
+            return;
+        }
+
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches() == false){
+            edEmailSecondAct.setError("Enter valid Email address!");
+            return;
+        }
+
+        if(TextUtils.isEmpty(passw)){
+            edPasswordSecondAct.setError("Password is required!");
+            return;
+        }
+
+        if(passw.length() < 6){
+            edPasswordSecondAct.setError("Password must not be less than 6 characters!");
+            return;
+        }
+
+        if(TextUtils.isEmpty(confirmPassw)){
+            edConfirmPasswordSecondAct.setError("Please, confirm your password!");
+            return;
+        }
+
+        if(confirmPassw.equals(passw) == false){
+            Toast.makeText(this, "Password and Confirm Password do not match, please try again",
+                    Toast.LENGTH_LONG).show();
+            edConfirmPasswordSecondAct.setText(null);
+            edConfirmPasswordSecondAct.requestFocus();
+            return;
+        }
+
+        //Register a new user in Firebase
+        fAuth.createUserWithEmailAndPassword(email,passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this,"User Created", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                } else {
+                    Toast.makeText(RegisterActivity.this,"Error! " + task.getException().getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 }
