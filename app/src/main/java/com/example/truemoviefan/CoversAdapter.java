cad3173e0337b9466be0ab34006b067813d59cbb
Fabricio Segovia <60413944+FabricioSe@users.noreply.github.com>
2022-12-12 15:27:10 -0500

    Fetch number of reviews
    fetched the whole reviews collection and verified if the imdbId of the review matched the imdbId of the poster that was selected

--- app/src/main/java/com/example/truemoviefan/CoversAdapter.java
+++ app/src/main/java/com/example/truemoviefan/CoversAdapter.java
@@ -11,6 +11,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -21,6 +27,9 @@ public class CoversAdapter extends RecyclerView.Adapter<CoversAdapter.ViewHolder
 
     private Context context;
     private List<Movie> movies;
+
+    FirebaseFirestore db;
+    int counter = 0;
 
     public CoversAdapter(List<Movie> movies) {
         this.movies = movies;
@@ -49,8 +58,21 @@ public class CoversAdapter extends RecyclerView.Adapter<CoversAdapter.ViewHolder
         // Set item views based on your views and data model
         holder.tvMovieTitle.setText(movie.getTitle());
 
-        // TODO: fetch number of reviews
-        holder.tvMovieRating.setText("X");
+        // Fetch number of reviews
+        db = FirebaseFirestore.getInstance();
+        Task<QuerySnapshot> dref = db.collection("review").get();
+        dref.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    if (document.getString("imdbId").equals(movie.getImdbID())) {
+                        counter = counter + 1;
+                    }
+                }
+                holder.tvMovieRating.setText(String.valueOf(counter) + " Reviews");
+                counter = 0;
+            }
+        });
 
         Picasso.with(context).load(movie.getPoster()).into(holder.ivMoviePoster);
 
